b239e757752294847343295092ca9d2895e89e46
Christopher Clemons <chris.clemons85@gmail.com>
2024-11-22 13:33:32 +0100

    feat: send override param to statsig

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,13 +2,22 @@ import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
+  const response = NextResponse.next()
+
   const ajsAnonymousId =
     request.cookies.get('ajs_anonymous_id')?.value ?? crypto.randomUUID()
 
-  const response = NextResponse.next()
   response.cookies.set('ajs_anonymous_id', ajsAnonymousId, {
     maxAge: /* 1 year */ 1000 * 365 * 60 * 60 * 24,
   })
+
+  const overrideParam = request.nextUrl.searchParams.get('o')
+
+  if (overrideParam) {
+    response.cookies.set('override', overrideParam, {
+      maxAge: /* 1 year */ 1000 * 365 * 60 * 60 * 24,
+    })
+  }
 
   return response
 }
