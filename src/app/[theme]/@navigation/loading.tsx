530100b3baf5e602641e42e63b26bd23977c8526
Christopher Clemons <chris.clemons85@gmail.com>
2024-12-04 21:51:34 +0100

    fix: remove header loaders, add id to navigation loader

--- src/app/[theme]/@navigation/loading.tsx
+++ src/app/[theme]/@navigation/loading.tsx
@@ -1,7 +1,10 @@
 export default function NavigationLoaging() {
   return (
     <div className="flex flex-col w-full gap-2">
-      <div className="flex w-full h-10 items-center px-2 justify-between">
+      <div
+        id="recent-projects"
+        className="flex w-full h-10 items-center px-2 justify-between"
+      >
         <div className="h-9 w-[254px] bg-zinc-200 dark:bg-zinc-800 animate-pulse rounded-sm" />
         <div className="h-full flex flex-col gap-[6px] justify-center">
           <div className="bg-zinc-200 dark:bg-zinc-800 animate-pulse h-[3px] w-7 place-self-start" />
